7cc8b3a7fc298cad2fdb44bf674f7c0f064ccaa2
Arpan Gupta <arpan.gupta@kuliza.com>
2017-05-31 12:10:20 +0530

    First commit for the package

---
+++ build/lib/drf_mockable/mockable.py
@@ -0,0 +1,33 @@
+from rest_framework.views import APIView
+
+class MockableView(APIView):
+    """
+    This is the class which will allow you to build Mockable APIs
+    """
+
+    def initial(self, request, *args, **kwargs):
+        """
+        Runs anything that needs to occur prior to calling the method handler.
+        """
+        self.format_kwarg = self.get_format_suffix(**kwargs)
+
+        # Perform content negotiation and store the accepted info on the request
+        neg = self.perform_content_negotiation(request)
+        request.accepted_renderer, request.accepted_media_type = neg
+
+        # Determine the API version, if versioning is in use.
+        version, scheme = self.determine_version(request, *args, **kwargs)
+        request.version, request.versioning_scheme = version, scheme
+
+        # Ensure that the incoming request is permitted
+        self.perform_authentication(request)
+        self.check_permissions(request)
+        self.check_throttles(request)
+        self.check_mockable(request)
+
+
+    def check_mockable(self, request):
+        """
+        """
+        if str(request.META.get('HTTP_MOCKABLE', "")).lower() == 'yes':
+            return self.mock_response
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(
+    name = 'drf_mockable',
+    packages = ['drf_mockable'], # this must be the same as the name above
+    version = '0.0.1',
+    description = 'A package to help in building mockable APIs based over Django Rest Framework (DRF)',
+    author = 'Arpan Gupta',
+    author_email = 'arpan29@gmail.com',
+    url = 'https://github.com/arpan29/drf-mockable', # use the URL to the github repo
+    download_url = 'https://github.com/arpan29/drf-mockable/archive/master.zip', # I'll explain this in a second
+    keywords = ['mock', 'DRF', 'mockable', 'APIs', 'REST', 'Django'], # arbitrary keywords
+    classifiers = [],
+    license = 'MIT',
+    install_requires=[
+        'djangorestframework',
+    ],
+    zip_safe=False
+)
